6573cad8d5f70eb3e9d6ce499fc3c4ba551bb65c
Brian Wangila <bwanjalah@gmail.com>
2022-06-15 15:21:41 +0300

    commit tree 9956b3985b427345a996251e4b94769856426e1b

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector("#searchByID");
+    console.log(input.value);
+
+
+    fetch('http://localhost:3000/movies')
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data);
+    
+    });
+
+    inputForm.reset()
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
